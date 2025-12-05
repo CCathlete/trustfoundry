2d1e036f1752038483be9c4cad612090f2438f41
ccathlete <ccathlete01@gmail.com>
2025-12-05 16:16:45 +0100

    Created the backend project.

---
+++ backend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    // The 'content' array specifies which files to scan for Tailwind utility classes.
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
